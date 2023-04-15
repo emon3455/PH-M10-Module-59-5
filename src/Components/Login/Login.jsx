4a649320c64b52885c1b3197814cc05d8e7cbcfc
Emon <emon.mhk69@gmail.com>
2023-04-15 20:42:57 +0600

    login and register page create done with toggle

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { getAuth } from "firebase/auth";
+import { app } from '../../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
+
+const auth = getAuth(app);
+
 
 const Login = () => {
+
+
+
     return (
-        <div>
-            <h2>Please Login</h2>
+        <div className='flex h-screen flex-col gap-10 items-center justify-center'>
+            <h2 className='text-4xl font-bold'>Please Login</h2>
+            <form className='flex flex-col gap-5'>
+                <input className='input-field' type="email"  name='email' id='email' placeholder='your email' required/>
+                <input className='input-field' type="password" name="password" id="password" placeholder='your pass'  required/>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>Don't Have Any Accounts ? <span><Link className='text-violet-600 font-semibold' to="/register">Click here To Create</Link></span></p>
         </div>
     );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='flex h-screen flex-col gap-10 items-center justify-center'>
+            <h2 className='text-4xl font-bold'>Please Register</h2>
+            <form className='flex flex-col gap-5'>
+                <input className='input-field' type="text" name='name' id='name' placeholder='your name'/>
+                <input className='input-field' type="email"  name='email' id='email' placeholder='your email' required/>
+                <input className='input-field' type="password" name="password" id="password" placeholder='your pass'  required/>
+                <input className='btn-submit' type="submit" value="Register" />
+            </form>
+            <p>Already Have An Accounts ? <span><Link className='text-violet-600 font-semibold' to="/login">Click here To Login</Link></span></p>
         </div>
     );
 };
