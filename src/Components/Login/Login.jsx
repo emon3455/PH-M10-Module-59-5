cb81ba55bb03be71fafd9e0f3f88b5ba19860f67
Emon <emon.mhk69@gmail.com>
2023-04-16 15:26:14 +0600

    log in with google done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { getAuth } from "firebase/auth";
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from '../../../firebase/firebase.config';
 import { Link } from 'react-router-dom';
 
 const auth = getAuth(app);
-
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
 
+    const [logUser , setUser] = useState(null);
 
+    const handleGoogleSignIn =()=>{
+        signInWithPopup(auth,googleProvider)
+        .then(res=>{
+            const logedUser = res.user;
+            console.log(logedUser);
+        })
+        .catch(er=>{
+            console.log(er.message);
+        })
+    }
 
     return (
         <div className='flex h-screen flex-col gap-10 items-center justify-center'>
@@ -19,6 +30,15 @@ const Login = () => {
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
             <p>Don't Have Any Accounts ? <span><Link className='text-violet-600 font-semibold' to="/register">Click here To Create</Link></span></p>
+
+            <div className="flex gap-10">
+                <button onClick={handleGoogleSignIn} className='btn-submit'>
+                    Login With Google 
+                </button>
+                <button className='btn-submit'>
+                    Login With Github
+                </button>
+            </div>
         </div>
     );
 };
